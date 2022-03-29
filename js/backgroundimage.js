60eb3cf7d5ffff5b7e27066667a92f1ecf2473dc
Kuxwpel <kuxwpel@gmail.com>
2022-03-29 16:18:37 +0200

    wsparcie dla grid, mobilna wersja, popup

--- js/backgroundimage.js
+++
@@ -1,7 +0,0 @@
-function menu_start_bg()
- {
-    if(document.getElementsByClassName('dropbtn'). clicked == true)
-    {
-    alert("button was clicked");
-    }
- }
--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,12 @@
-function start() {
-    document.getElementById("menu_start").classList.toggle("show");
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function startON() {
+  document.getElementById("menu-start").classList.toggle("show");
 }
+
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
-
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
@@ -14,5 +16,4 @@ window.onclick = function(event) {
       }
     }
   }
-}
-
+} 
